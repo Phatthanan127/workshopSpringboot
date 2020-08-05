97dd1fba2d0a766ec45481f7f910b8507a8c5b47
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-05 14:52:33 +0700

    step_03.1 postman

---
+++ src/main/java/com/example/demo/NewUserRequest.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+public class NewUserRequest {
+    private String name;
+    private int age;
+
+    public NewUserRequest(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
