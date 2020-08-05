76a098385f96a8b3bf3bf37452fb53f590bcd54d
Phatthanan Rittichai <phatthananrit@mathregit.group>
2020-08-05 11:39:26 +0700

    step_01 add pathVariable

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,17 +1,25 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UsersResponse[] getAllUser() {
-        UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1 , "User1");
-        usersResponses[1] = new UsersResponse(2 , "User2");
-        return usersResponses;
-
+    public List<UsersResponse>  getAllUser() {
+        List<UsersResponse> usersResponsesList = new ArrayList<>();
+        usersResponsesList.add(new UsersResponse(1,"User 1"));
+        usersResponsesList.add(new UsersResponse(2,"User 2"));
+        usersResponsesList.add(new UsersResponse(3,"User 3"));
+        return usersResponsesList;
+    }
+    @GetMapping("/users/{id}")
+    public  UsersResponse getUserByID(@PathVariable  int id){
+        return  new UsersResponse(id,"User"+id);
     }
 }
